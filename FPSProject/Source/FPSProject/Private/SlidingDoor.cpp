745f4bac424620f3df252638f83efc68c40bf846
Ali El Saleh <elsaleh78@gmail.com>
2018-10-05 22:40:33 +1000

    Added SlidingDoor C++ Actor
    Ignored large binary files
    Added CurveFloat asset for timeline

---
+++ FPSProject/Source/FPSProject/Private/SlidingDoor.cpp
@@ -0,0 +1,95 @@
+// Copyright © 2018, AJACKS
+
+#include "SlidingDoor.h"
+
+ASlidingDoor::ASlidingDoor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Root Component
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root Scene"));
+	RootComponent = Root;
+
+	// Static Mesh Component
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
+	StaticMeshComponent->SetupAttachment(RootComponent);
+
+	// Static Mesh
+	Door = Cast<UStaticMesh>(StaticLoadObject(UStaticMesh::StaticClass(), nullptr, TEXT("StaticMesh'/Game/Geometry/Meshes/1M_Cube.1M_Cube'")));
+	if (Door != nullptr)
+	{
+		StaticMeshComponent->SetStaticMesh(Door);
+		StaticMeshComponent->SetWorldLocation(FVector(0, 0, 0));
+		StaticMeshComponent->SetWorldScale3D(FVector(0.5, 4.25, 3.5));
+	}
+
+	// Trigger Box Component
+	TriggerBoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger Box Component"));
+	TriggerBoxComponent->SetupAttachment(StaticMeshComponent);
+	TriggerBoxComponent->SetWorldLocation(FVector(-150, 0, 0));
+	TriggerBoxComponent->SetBoxExtent(FVector(50, 40, 50));
+	TriggerBoxComponent->SetCollisionProfileName(FName("Trigger"));
+	TriggerBoxComponent->OnComponentBeginOverlap.AddDynamic(this, &ASlidingDoor::Slide);
+
+	// Timeline Component
+	Timeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("Timeline Component"));
+
+	// Curve Float
+	CurveFloat = Cast<UCurveFloat>(StaticLoadObject(UCurveFloat::StaticClass(), nullptr, TEXT("CurveFloat'/Game/5329_Goblins/DoorSlideTime.DoorSlideTime'")));
+	
+	// Other settings
+	this->bCanBeDamaged = false;
+}
+
+// Called when the game starts or when spawned
+void ASlidingDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Timeline Setup
+	FOnTimelineFloat OnTimelineCallback;
+
+	OnTimelineCallback.BindUFunction(this, "Slide");
+
+	if (CurveFloat != nullptr)
+	{
+		Timeline = NewObject<UTimelineComponent>(this, FName("DoorAnimation"));
+		Timeline->CreationMethod = EComponentCreationMethod::UserConstructionScript;
+		Timeline->SetPropertySetObject(this);
+		Timeline->SetLooping(false);
+		Timeline->SetPlaybackPosition(0.0f, false);
+		Timeline->SetPlayRate(Speed);
+		Timeline->AddInterpFloat(CurveFloat, OnTimelineCallback);
+		Timeline->SetTimelineLength(Speed);
+		Timeline->RegisterComponent();
+	}
+
+	// Door
+	CurrentLocation = StaticMeshComponent->GetComponentLocation();
+}
+
+void ASlidingDoor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	if (Timeline != nullptr)
+		Timeline->TickComponent(DeltaTime, LEVELTICK_TimeOnly, nullptr);
+}
+
+void ASlidingDoor::Slide(class UPrimitiveComponent* HitComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	Timeline->Play();
+
+	const float TimelineValue = Timeline->GetPlaybackPosition();
+	const float CurveFloatValue = CurveFloat->GetFloatValue(TimelineValue);
+
+	if (Sideways)
+		EndLocation = FVector(CurrentLocation.X, CurrentLocation.Y - YSlide, CurrentLocation.Z);
+	else
+		EndLocation = FVector(CurrentLocation.X, CurrentLocation.Y, CurrentLocation.Z - ZSlide);
+
+	StaticMeshComponent->SetWorldLocation(FMath::Lerp(CurrentLocation, EndLocation, CurveFloatValue));
+
+	if (IsValid(TriggerBoxComponent))
+		TriggerBoxComponent->DestroyComponent();
+}
---
+++ FPSProject/Source/FPSProject/Public/SlidingDoor.h
@@ -0,0 +1,60 @@
+// Copyright © 2018, AJACKS
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/TimelineComponent.h"
+#include "Engine/StaticMesh.h"
+#include "Kismet/GameplayStatics.h"
+#include "SlidingDoor.generated.h"
+
+UCLASS(hidecategories = ("Input", "Actor", "HLOD"))
+class FPSPROJECT_API ASlidingDoor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ASlidingDoor();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+		void Slide(class UPrimitiveComponent* HitComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+private:
+	USceneComponent* Root = nullptr;
+	
+	UPROPERTY(EditInstanceOnly)
+		UStaticMeshComponent* StaticMeshComponent = nullptr;
+
+	UStaticMesh* Door = nullptr;
+	
+	UPROPERTY(EditInstanceOnly)
+		UBoxComponent* TriggerBoxComponent = nullptr;
+
+	UPROPERTY(EditInstanceOnly)
+		UTimelineComponent* Timeline = nullptr;
+
+	UPROPERTY(EditInstanceOnly)
+		UCurveFloat* CurveFloat = nullptr;
+
+	UPROPERTY(EditInstanceOnly, Category = "Slide", meta = (ToolTip = "Should the door slide sideways?"))
+		bool Sideways = false;
+
+	UPROPERTY(EditInstanceOnly, Category = "Slide", meta = (ToolTip = "How far should the door slide on the Z-Axis?", EditCondition = "Sideways"))
+		float YSlide = 300;
+
+	UPROPERTY(EditInstanceOnly, Category = "Slide", meta = (ToolTip = "How far should the door slide on the Z-Axis?", EditCondition = "!Sideways"))
+		float ZSlide = 300;
+
+	UPROPERTY(EditInstanceOnly, Category = "Slide", meta = (ToolTip = "How fast should the door slide? 0 = no movement, 1 = normal, 2+ = fast"))
+		float Speed = 1;
+
+	FVector CurrentLocation = FVector(0, 0, 0);
+	FVector EndLocation = FVector(0, 0, 0);
+};
